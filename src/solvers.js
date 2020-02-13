a02259024651536ba221b907a25f0803c746dbfc
Steven Papalia <stevenapapalia@yahoo.com>
2020-02-12 22:00:45 -0800

    Found 1 solution for nRooks, tried allNRookSolution

--- src/solvers.js
+++ src/solvers.js
@@ -16,16 +16,77 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
 
+
+  // var solution = new Board({n:n});
+  // var addRookToRow = function(currentRow) {
+  //   if (currentRow === n - 1) {
+  //     return;
+  //   }
+  //   for (var column = 0; column < n; column++) {
+  //     board.rows()[currentRow][column] = 1;
+  //     // check if conflict occurs
+  //     for (var colCheck = column; colCheck < n + 1; colCheck++) {
+  //       // if it does 
+  //       if(board.hasAnyColConflicts()) {
+  //         // remove conflict
+  //         board.rows()[currentRow][colCheck] = 0;
+  //         // if it is a valid square add 1 there
+  //         if (colCheck < n) {
+  //           board.rows()[currentRow][colCheck + 1] = 1;
+  //         }
+  //       } else {
+  //         break;
+  //       }
+  //     }
+  //     addRookToRow(currentRow + 1);
+  //   }
+  //   return solution;
+
+  // }
+
+
+
+
+
+  var solution = [];
+  for (var i = 0; i < n; i++) {
+    solution.push([]);
+    for (var j = 0; j < n; j++) {
+      if (i === j) {
+        solution[i].push(1);
+      } else {
+        solution[i].push(0); 
+      }
+    }
+  }
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
-
+  var solutionCount = 0;
+  var board = new Board({n: n});
+  var addRookToRow = function(currentRow) {
+    // check if any conflicts occur every round
+    if (currentRow === n - 1) {
+      solutionCount++;
+      // if (!hasAnyRowConFlicts && !hasAnyColConflicts) {solution.push}
+      return;
+    }
+    for (var column = 0; column < n; column++) {
+      board.rows()[currentRow][column] = 1;
+      // check if conflict occurs
+      // if it does ... board.rows()[currentRow][column] = 0;
+      // add peice to board.row()[column+1] if still a problem keep going until column+1===n then move on to next row
+      addRookToRow(currentRow + 1);
+      // board.rows()[currentRow][column] = 0;
+    }
+  };
+  addRookToRow(0);
+  console.log(solutionCount + 'num');
+  console.log(board);
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
